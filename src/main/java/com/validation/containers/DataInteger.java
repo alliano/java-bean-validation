8602ac272e98ee8b97a84320e474f579add31786
alliano <allianoanonymous@gmail.com>
2023-03-29 15:55:52 +0900

    crate custom tipe data

---
+++ src/main/java/com/validation/containers/DataInteger.java
@@ -0,0 +1,14 @@
+package com.validation.containers;
+
+public class DataInteger {
+    
+    private Integer integer;
+
+    public Integer getInteger() {
+        return integer;
+    }
+
+    public void setInteger(Integer integer) {
+        this.integer = integer;
+    }
+}
